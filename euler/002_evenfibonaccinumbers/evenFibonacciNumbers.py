ff35859b2580ba7e2b9fe2bb7a71b89f23a06b6a
Gerardo Garzón Villar <gerardogv20@gmail.com>
2019-06-28 08:20:34 -0500

    Fixed loop condition

--- euler/002_evenfibonaccinumbers/evenFibonacciNumbers.py
+++ euler/002_evenfibonaccinumbers/evenFibonacciNumbers.py
@@ -1,7 +1,7 @@
 fibo = [1, 1]
 evenFibonacciSum = 0
 
-while fibo[0] + fibo[1] < 1000000:
+while fibo[0] + fibo[1] < 4000000:
 	fibo[0], fibo[1] = fibo[1], fibo[0] + fibo[1]
 	if fibo[1] % 2 == 0:
 		evenFibonacciSum += fibo[1]
