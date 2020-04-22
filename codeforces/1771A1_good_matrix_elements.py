a0f610c0128270ed3a67096f081dfa96071f088f
Gerardo Garzón Villar <gerardogv20@gmail.com>
2020-04-22 10:03:08 -0500

    Create 1771A1_good_matrix_elements.py

---
+++ codeforces/1771A1_good_matrix_elements.py
@@ -0,0 +1,27 @@
+# matrix dimensions
+n = int(input())
+
+matrix = []
+
+for mr in range(0, n):
+	matrix.append(list(map(int, input().split())))
+
+# get middle row (no need for + 1 since index starts with 0)
+# good matrix points
+gmp = sum(matrix[(n//2)])
+matrix[n//2] = [0] * n
+
+# get middle column
+gmp += sum([row[n//2] for row in matrix])
+
+for x in range(0, n):
+	matrix[x][n//2] = 0
+
+# get main and secondary diagonal
+# since all elements already counted for middle row
+# and middle column are already 0
+# there's no need to worry about [n/2][n/2] now
+for y in range(0, n):
+	gmp += matrix[y][y] + matrix[-1 - y][y]
+
+print(gmp)
